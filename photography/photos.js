56f88a36f56a2dc58c6b4728c1273395ec759a6a
jaychempan <1241762712@qq.com>
2023-11-28 19:40:58 +0800

    update photos

--- photography/photos.js
+++ photography/photos.js
@@ -1,7 +1,7 @@
 
 var photos = {
     "projects": [
-        {"name": "Hangzhou", "photos": 7},
+        {"name": "Hangzhou", "photos": 5},
         {"name": "Beijing", "photos": 6},
         {"name": "Greece", "photos": 9},
     ],
